f460140173870c0211b1b75e47a84f5581ad8754
aduonye iwarimie <aduonyeiwarimie@gmail.com>
2019-03-26 19:56:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// write your code below!
+// wrte your code below!
+function happyHolidays() {
+  return "happy holidays!"
+}
+function happyHolidaysTo(holiday, firstName) {
+  return `Happy ${Easter}, ${Chinyere}!`
+}
+function happyHolidays('Chinyere') {
+  return `${Happy holidays}, ${Chinyere}!`
+}
+function holidayCountdown(easter, remainingDays) {
+  return `It's ${'6 days'} days until ${easter}!`
+}
